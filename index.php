9c553b81650dafd1bc296a42538f3933f51853e7
bi-suhas <bi.vpsuhas@gmail.com>
2022-06-09 21:07:24 +0530

    first commit

--- index.php
+++ index.php
@@ -23,11 +23,7 @@
     <hr>
     <p>Financial Year Start : <span id="start"></span></p>
     <p>Financial Year End : <span id="end"></span></p>
-    <?php
-        function getCountryFinancialYear($country,$year){
 
-        }
-    ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
         $( document ).ready(function() {
